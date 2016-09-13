51245df43d792ab26b4401a904c2d28ceb90e9c6
Andrew <atandiawan@gmail.com>
2016-09-13 11:33:18 +0700

    Release 0

--- student_roster.js
+++ student_roster.js
@@ -3,39 +3,73 @@
 const repl = require('repl'); // optional
 const sqlite = require('sqlite3').verbose();
 
-class Controller {
-  
+let file = 'student.db'
+let db = new sqlite.Database(file)
 
+class Controller {
   static addStudent(firstname, lastname, birthdate) {
-    let CREATE_TABLE = "INSERT INTO student (firstname, lastname, birthdate) values (" + firstname + ", " + lastname + ", " + birthdate + ");"
+    let ADD_STUDENT = `INSERT INTO student (firstname, lastname, birthdate) VALUES ('${firstname}', '${lastname}', '${birthdate}')`
+    Controller.runDbCommand(ADD_STUDENT)
   }
 
-  static deleteStudent() {
-
+  static deleteStudent(id) {
+    let DELETE_STUDENT = `DELETE FROM student WHERE id = ${id}`
+    Controller.runDbCommand(DELETE_STUDENT)
   }
 
   static displayStudent() {
-
+    let DISPLAY_STUDENT = `SELECT * FROM student`
+    Controller.runDbAllCommand(DISPLAY_STUDENT)
   }
 
-  static displayStudentByName() {
-
+  static displayStudentByName(firstname, lastname) {
+    let DISPLAY_STUDENT_BY_NAME = `SELECT * FROM student WHERE firstname = ${firstname} AND lastname = ${lastname}`
+    Controller.runDbCommand(DISPLAY_STUDENT_BY_NAME)
   }
 
   static displayStudentByAttribute() {
 
   }
 
-  static displayStudentByBirthdayMonth() {
-
+  static displayStudentByBirthdayMonth(month) {
+    let DISPLAY_STUDENT_BY_BIRTHDAY_MONTH = `SELECT * FROM student WHERE MONTH(birthdate) = ${month}`
+    Controller.runDbCommand(DISPLAY_STUDENT_BY_BIRTHDAY_MONTH)
   }
 
   static displayStudentSortBirthday() {
-
+    let DISPLAY_STUDENTS_SORT_BIRTHDAY = `SELECT * FROM student ORDER BY birthdate`
+    Controller.runDbCommand(DISPLAY_STUDENTS_SORT_BIRTHDAY)
   }
+
+  static runDbCommand(command){
+    db.serialize(function() {
+      // Create table
+      db.run(command, function(err,data) {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log("Successful")
+          console.log(data)
+        }
+      })
+    })
+  }
+
+  static runDbAllCommand(command){
+      db.serialize(function() {
+        // Create table
+        db.all(command, function(err,data) {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log(data)
+          }
+        })
+      })
+    }
 }
-
-
+let replServer = repl.start({prompt:'>'})
+replServer.context.Controller = Controller
 
 // Create Student class to CRUD with Database
 // Create many methods for Student class to execute SQL commands
