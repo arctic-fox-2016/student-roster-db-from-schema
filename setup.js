a99a89688e71f1674b265365da1927bbffb8fe8d
Peter Raswono <email": null>
2016-09-13 10:32:00 +0700

    Andrew Peter

--- setup.js
+++ setup.js
@@ -1,3 +1,48 @@
 "use strict"
 
-//write your code here
+const repl = require('repl')
+const sqlite = require('sqlite3').verbose()
+
+let file = 'student.db'
+let db = new sqlite.Database(file)
+
+// SQL Statement
+let CREATE_TABLE = "CREATE TABLE IF NOT EXISTS student ( id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT NOT NULL, lastname TEXT, birthdate DATE )"
+let SEED_DATA = "INSERT INTO student (firstname, lastname, birthdate) VALUES ('Rubi', 'Henjaya', '1986-11-20'), ('Riza', 'Fahmi', '1981-12-31')"
+
+// CREATE_TABLE
+let createTable = () => {
+
+  // Run SQL one at a time
+  db.serialize(function() {
+
+    // Create table
+    db.run(CREATE_TABLE, function(err) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('CREATE TABLE')
+      }
+    })
+  })
+}
+
+// SEED_DATA
+let seedData = () => {
+
+  // Run SQL one at a time
+  db.serialize(function() {
+
+    // Insert data into datatable
+    db.run(SEED_DATA, function(err) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('INSERT DATA')
+      }
+    })
+  })
+}
+
+createTable()
+seedData()
--- student_roster.js
+++ student_roster.js
@@ -3,4 +3,48 @@
 const repl = require('repl'); // optional
 const sqlite = require('sqlite3').verbose();
 
-// write your code here
+class Controller {
+  
+
+  static addStudent(firstname, lastname, birthdate) {
+    let CREATE_TABLE = "INSERT INTO student (firstname, lastname, birthdate) values (" + firstname + ", " + lastname + ", " + birthdate + ");"
+  }
+
+  static deleteStudent() {
+
+  }
+
+  static displayStudent() {
+
+  }
+
+  static displayStudentByName() {
+
+  }
+
+  static displayStudentByAttribute() {
+
+  }
+
+  static displayStudentByBirthdayMonth() {
+
+  }
+
+  static displayStudentSortBirthday() {
+
+  }
+}
+
+
+
+// Create Student class to CRUD with Database
+// Create many methods for Student class to execute SQL commands
+
+// Write Javascript code to complete tasks
+// 1. Add student
+// 2. Delete student
+// Display all students
+// Display students that has name
+// Display students with attribute
+// Display students with birthday this month
+// Display students based on birthday sort
