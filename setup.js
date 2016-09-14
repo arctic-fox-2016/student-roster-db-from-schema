a19f1cbb656d6dd3b8a874597513717839e7ea62
Ari Adiprana <email": null>
2016-09-14 18:25:39 +0700

    release

--- setup.js
+++ setup.js
@@ -29,7 +29,7 @@ let insertStudent = () => {
   db.run(INSERT_DATA,{
     $FNAME: faker.name.firstName(),
     $LNAME: faker.name.lastName(),
-    $BIRTHDATE: faker.date.past()
+    $BIRTHDATE: faker.date.month()
   })
 }
 
@@ -63,6 +63,40 @@ let selectAllStudent = () => {
   })
 }
 
+let selectStudentWithSpecName = () => {
+  let SQLQuery = "SELECT * FROM STUDENT WHERE FIRSTNAME LIKE 'A%'"
+  db.all(SQLQuery, function(err, data) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(`=======================`)
+      console.log(`Daftar Student Berawalan A`)
+      console.log(`=======================`)
+      console.log(`ID - First Name - Last Name - Cohort Name`)
+      for(let idx in data){
+        console.log(`${data[idx].ID} - ${data[idx].FIRSTNAME} - ${data[idx].LASTNAME} - - ${data[idx].BIRTHDATE}`);
+      }
+    }
+  })
+}
+
+let selectStudentWithBirthDate = () => {
+  let SQLQuery = "SELECT ID, FIRSTNAME, LASTNAME, BIRTHDATE FROM STUDENT WHERE BIRTHDATE ='February'"
+  db.all(SQLQuery, function(err, data) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(`=======================`)
+      console.log(`Daftar Student Lahir Bulan Februari`)
+      console.log(`=======================`)
+      console.log(`ID - First Name - Last Name - Cohort Name`)
+      for(let idx in data){
+        console.log(`${data[idx].ID} - ${data[idx].FIRSTNAME} - ${data[idx].LASTNAME} - ${data[idx].BIRTHDATE}`);
+      }
+    }
+  })
+}
+
 //--------------------------------------------------- driver code ----------------------------
 //create table
 createTable()
@@ -74,5 +108,12 @@ for(let i=1;i<100;i++){
 
 //3. select all student
 selectAllStudent()
+
+//4&5. select name with firstname started with 'A%'
+selectStudentWithSpecName()
+
+//6
+selectStudentWithBirthDate()
+
 //2. delete all student
 deleteAllStudent()
